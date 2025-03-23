48e656870803802a606bda188143f8100b10aaf5
MatskewitchAnna <asm160704@gmail.com>
2025-03-23 21:20:51 +0300

    rename classes

--- src/test/java/pages/BrowserSettings.java
+++ src/test/java/pages/BrowserSettings.java
@@ -1,4 +1,4 @@
-package task1;
+package pages;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
--- src/test/java/pages/MainPageTest.java
+++ src/test/java/pages/MainPageTest.java
@@ -1,4 +1,4 @@
-package task1;
+package pages;
 
 import org.junit.Test;
 
